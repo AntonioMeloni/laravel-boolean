15a3d6ca7be35b76cf73e6bfd7ce81da0da3a295
Antonio <ameloni9@gmail.com>
2020-06-03 18:11:30 +0200

    Migrations

---
+++ database/factories/CatgoryFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Catgory;
+use Faker\Generator as Faker;
+
+$factory->define(Catgory::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
